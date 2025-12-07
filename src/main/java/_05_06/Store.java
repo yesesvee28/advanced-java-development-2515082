6b6d5cfe893595da7477f17c4d098d61e41ca9cd
yesesvee28 <chinnuyesesvee@gmail.com>
2025-12-07 09:17:49 +0530

    _05_06 completed

--- src/main/java/_05_06/Store.java
+++ src/main/java/_05_06/Store.java
@@ -1,9 +1,12 @@
 package _05_06;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Store {
 
     // Make this method a synchronized method
-    static void purchase(StockChecker stockChecker, int amount) {
+    static synchronized void purchase(StockChecker stockChecker, int amount) {
         int stock = stockChecker.getStock();
         if(stock - amount < 0) {
             System.out.println("Out of stock");
@@ -18,16 +21,20 @@ public class Store {
     public static void main(String[] args) {
 
         // Create a new StockChecker object.
-
+        StockChecker stockChecker=new StockChecker();
         // Create an ExecutorService with a fixed thread pool of 4 threads
-
+        ExecutorService executorService=Executors.newFixedThreadPool(4);
         // Submit 4 tasks to the executor service.
-
         // In each task you should call the purchase method and pass in 20,
         // to represent a customer purchasing 20 items.
+        executorService.submit(()-> purchase(stockChecker,20));
+        executorService.submit(()-> purchase(stockChecker,20));
+        executorService.submit(()-> purchase(stockChecker,20));
+        executorService.submit(()-> purchase(stockChecker,20));
+        
 
         // Shut down the executor service.
-
+        executorService.shutdown();
     }
 
 }
