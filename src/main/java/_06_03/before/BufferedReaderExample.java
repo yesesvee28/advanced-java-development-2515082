78c04692cb8cda4571bd14b80ed55c6ba0f3e38a
yesesvee28 <chinnuyesesvee@gmail.com>
2025-12-09 13:57:09 +0530

    completed _06

--- src/main/java/_06_03/before/BufferedReaderExample.java
+++ src/main/java/_06_03/before/BufferedReaderExample.java
@@ -1,9 +1,26 @@
 package _06_03.before;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BufferedReaderExample {
 
     public static void main(String[] args) {
+        //The difference between try and try with resources is that:
+        // In try we read the file inside the try block but in try with resourses it read the file before the going inside
+        // below is the example for try with resourses.
+        try(BufferedReader reader=new BufferedReader(new FileReader("src/main/java/_06_03/example.txt"))){
+            String firstLine=reader.readLine();
+            System.out.println("First Line:");
+            System.out.println(firstLine);
 
-
+            StringBuilder stringBuilder=new StringBuilder();
+            reader.lines().forEach(line->stringBuilder.append(line+"\n"));
+            System.out.println("Rest of the lines:");
+            System.out.println(stringBuilder);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/_06_05/FileReaderExercise.java
+++ src/main/java/_06_05/FileReaderExercise.java
@@ -1,19 +1,35 @@
 package _06_05;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileReaderExercise {
 
     // This method should return the first line of BufferedReaderVsScanner.txt.
     String getFirstLine() {
         String firstLine = "";
-
+        try(BufferedReader reader=new BufferedReader(new FileReader("src/main/java/_06_05/BufferedReaderVsScanner.txt"))){
+            firstLine=reader.readLine();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         return firstLine;
     }
 
     // This method should return all of the content of BufferedReaderVsScanner.txt
     // as a single String.
     String getWholeFile() {
+        String firstLine = "";
         String wholeFile = "";
-
+        try(BufferedReader reader=new BufferedReader(new FileReader("src/main/java/_06_05/BufferedReaderVsScanner.txt"))){
+            firstLine=reader.readLine();
+            StringBuilder builder=new StringBuilder();
+            reader.lines().forEach(line->builder.append(line+""));
+            wholeFile=builder.toString();
+        }catch(IOException e){
+            e.printStackTrace();
+        }       
         return wholeFile;
     }
 
