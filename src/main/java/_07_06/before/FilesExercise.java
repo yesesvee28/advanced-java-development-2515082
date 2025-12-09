d10ef88c3487a9d6b6fd9f0f86110eca071ffef9
yesesvee28 <chinnuyesesvee@gmail.com>
2025-12-09 23:23:53 +0530

    Completed _07

--- src/main/java/_07_06/before/FilesExercise.java
+++ src/main/java/_07_06/before/FilesExercise.java
@@ -1,15 +1,27 @@
 package _07_06.before;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.Files;
 public class FilesExercise {
 
     public static void main(String[] args) {
-        // Create a new empty file called example.txt inside folderA
+        try{
+             // Create a new empty file called example.txt inside folderA
+                Path path=Paths.get("/Users/yesesvee/Desktop/advanced-java-development-2515082/src/main/java/_07_06/folderA/example.txt");
+                if(Files.notExists(path)){
+                    Files.createFile(path);
+                }
 
-        // Copy the file to folderB
-
-        // List the contents of folderB to check that your file is in there
-
+             // Copy the file to folderB
+                Path pathToNewLocation=Paths.get("/Users/yesesvee/Desktop/advanced-java-development-2515082/src/main/java/_07_06/folderB/example.txt");
+                if(Files.notExists(pathToNewLocation)){
+                    Files.copy(path,pathToNewLocation);
+                }
+              // List the contents of folderB to check that your file is in there
+                Files.list(Paths.get("/Users/yesesvee/Desktop/advanced-java-development-2515082/src/main/java/_07_06/folderB"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
-
-
 }
